5290ad371c20227ecda799da7aea2007fd6c461a
Sangkyu Han <33011722+hangoon2@users.noreply.github.com>
2020-11-20 14:28:39 +0900

    Update videoprocess.rb

--- videoprocess.rb
+++ videoprocess.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Videoprocess < Formula
-  homepage 'https://https://github.com/hangoon2/VideoProcess'
-  head 'https://https://github.com/hangoon2/VideoProcess', :using => :git
+  homepage 'https://github.com/hangoon2/VideoProcess'
+  head 'https://github.com/hangoon2/VideoProcess', :using => :git
 
-  url 'https://https://github.com/hangoon2/VideoProcess/releases/download/1.0.0/nxptc_28_vd'
+  url 'https://github.com/hangoon2/VideoProcess/releases/download/1.0.0/nxptc_28_vd'
 
   def install
     bin.install 'videoprocess'
